6b3207276721a1b572d0cd920d81384b4f3728cd
akefirad <akefirad@gmail.com>
2025-05-08 23:00:17 +0200

    initial commit
    add private
    
    add alf

---
+++ home/dot_config/fish/conf.d/abbrs.fish
@@ -0,0 +1,22 @@
+abbr --add g     "git";
+abbr --add gito  "git checkout";
+abbr --add gitp  "git push";
+abbr --add gits  "git status";
+abbr --add main  "git checkout main && git pull --all";
+
+abbr --add tf    "terraform";
+abbr --add lzg   "lazygit";
+abbr --add lzdc  "lazydocker";
+abbr --add npr   "npm run";
+
+abbr --add py     "python3";
+abbr --add python "python3";
+
+abbr --add argo  "argocd";
+abbr --add istio "istioctl";
+abbr --add redis "redis-cli";
+abbr --add fork  "open -a Fork";
+abbr --add lapce "open -a Lapce";
+
+abbr --add gradle "gradlew";
+abbr --add mvn    "mvnw";
